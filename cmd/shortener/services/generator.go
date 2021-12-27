95a3d2a51f888d5af0f93210e7b6ee3b453db1d0
2heoh <sergey.lobin@gmail.com>
2021-12-27 15:46:48 +0300

    After linter

--- cmd/shortener/services/generator.go
+++ cmd/shortener/services/generator.go
@@ -9,11 +9,11 @@ type Generator interface {
 	Generate(string) string
 }
 
-type IDGenerator struct {
-}
+type IDGenerator struct{}
 
 func (idgen *IDGenerator) Generate(url string) string {
 	crc32q := crc32.MakeTable(0xD5828281)
+
 	return fmt.Sprintf("%08x", crc32.Checksum([]byte(url), crc32q))
 }
 
