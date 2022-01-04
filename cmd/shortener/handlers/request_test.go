e22e08007c63879c83264d1047043b91fc357e9f
2heoh <sergey.lobin@gmail.com>
2022-01-04 18:58:36 +0300

    base url w/o trailing "/"

--- cmd/shortener/handlers/request_test.go
+++ cmd/shortener/handlers/request_test.go
@@ -191,7 +191,7 @@ func TestRequestHandler(t *testing.T) {
 	for _, tt := range tests {
 
 		t.Run(tt.name, func(t *testing.T) {
-			r := handlers.NewHandler(testURLService, "http://test/")
+			r := handlers.NewHandler(testURLService, "http://test")
 			ts := httptest.NewServer(r)
 			defer ts.Close()
 			req, err := http.NewRequest(tt.request.method, ts.URL+tt.request.path, tt.request.body)
